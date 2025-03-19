45d3ae5b4e3bf9f83d8b01494aa29935b66305ca
miriambyrne <115432228+miriambyrne@users.noreply.github.com>
2025-03-19 14:39:06 +0000

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,51 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
-  const translator = new Translator();
+
+  let translator = new Translator();
+
 
   app.route('/api/translate')
     .post((req, res) => {
-      
-    });
+      let string = req.body.text;
+      let locale = req.body.locale;
+      //american-to-british, british-to-american
+      if (!locale || !('text' in req.body)) {
+        res.json({error: "Required field(s) missing"})
+      }
+      else if (string == "") {
+        res.json({ error: "No text to translate" })
+      }
+
+      else if (!(locale == "american-to-british" || locale == "british-to-american")) {
+        res.json({ error: "Invalid value for locale field" })
+      }
+
+      else {
+        if(locale == "american-to-british"){
+        let result = translator.americanToBritish(string, locale)
+
+        if (result == string) {
+          res.json({text: string, translation: "Everything looks good to me!"})
+        }
+        else {
+        res.json({text: string, translation: result})
+        }
+        }
+
+        else if(locale == "british-to-american"){
+          let result = translator.britishToAmerican(string, locale)
+          if (result == string) {
+            res.json({text: string, translation: "Everything looks good to me!"})
+          }
+          else {
+          res.json({text: string, translation: result})
+          }
+          }
+      }
+      })
+
+
+
 };
+
